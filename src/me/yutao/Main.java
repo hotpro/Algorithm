d6aa11308553faa55e1f02b35bb0e90a34233437
chris.hou <chrishou118@gmail.com>
2015-07-07 13:29:10 -0700

    copy books problem

--- src/me/yutao/Main.java
+++ src/me/yutao/Main.java
@@ -1,8 +1,18 @@
 package me.yutao;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        int[] nums = new int[] {1, 2};
+        changeArray(nums);
+        System.out.println(Arrays.toString(nums));
     }
+
+    public static void changeArray(int[] nums) {
+        nums[0] = 3;
+    }
+
 }
--- src/me/yutao/ninechapter/nine1_bs/FindBadVersion.java
+++ src/me/yutao/ninechapter/nine1_bs/FindBadVersion.java
@@ -3,6 +3,8 @@ package me.yutao.ninechapter.nine1_bs;
 /**
  * Created by yutao on 7/6/15.
  *
+ * Find Bad Version
+ *
  * 查找代码库的第一个坏版本。有一个SVN代码库，版本号从1到N，自从某个某人提交了他的代码之后，代码库就坏掉了程序跑不起来了，
  * 也就是自从某人提交的这个版本及之后的所有版本都是坏的代码版本。现在有一个接口，isBadVersion(int version)，
  * 可以用于判断version这个编号的代码是否是好的。请你利用这个接口，来找到某人提交的那个坏版本的编号。即，找到第一个i，
